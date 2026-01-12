b70e03735839dccb7f17cecb6bd7d7d0cff8945b
Roshankumar404 <roshankuar951@gmail.com>
2026-01-12 13:21:42 +0530

    wrote the networking code using the okhttp3

--- app/src/main/java/com/example/livetv/Data/Repo/RepoLink.kt
+++ app/src/main/java/com/example/livetv/Data/Repo/RepoLink.kt
@@ -1,4 +1,25 @@
 package com.example.livetv.Data.Repo
 
-class RepoLink {
+import com.example.livetv.Data.Model.Channel
+import com.example.livetv.utils.VideoParser
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+class RepoLink{
+    private  val client= OkHttpClient()
+    suspend fun fetchSportsChannels(): List<Channel> =
+        withContext(Dispatchers.IO){
+            val request = Request.Builder()
+                .url("https://iptv-org.github.io/iptv/index.m3u")
+                .build()
+            val response= client.newCall(request).execute()
+            val body= response.body?.string().orEmpty()
+            VideoParser.parse(body)
+                .filter {
+                    it.group?.lowercase()?.contains("sport")==true
+                }
+
+    }
 }
